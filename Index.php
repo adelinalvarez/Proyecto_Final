079b9e9d3c6cb652175bc016d83b3208e7f7d74e
adelinalvarez <144082099+adelinalvarez@users.noreply.github.com>
2023-09-18 17:50:49 -0400

    style

--- Index.php
+++ Index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Restaurante Doña Hilda</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="Css/style.css">
 
 </head>
 <body>
