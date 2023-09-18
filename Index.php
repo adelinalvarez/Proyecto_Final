0ef7749dec6b8ffa2662af6a78cdb93b9dbd3615
adelinalvarez <144082099+adelinalvarez@users.noreply.github.com>
2023-09-17 21:06:55 -0400

    Iniciar

---
+++ Index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Restaurante Doña Hilda</title>
+    <link rel="stylesheet" href="css/estilos.css">
+
+</head>
+<body>
+     <header class="header">
+             <div class="menu container">
+                
+                <label for="menu">
+                <img src="images/menu.png" class="menu-icono" alt="menu">
+                </label>
+                <nav class="navbar">
+                    <ul>
+                       
+                        <li> <a href="Nosotros.html">Nosotros</a></li>
+                       
+                        <li><a href="Contacto.html">Contacto</a></li>
+                        <li><a href="#" class="logo"><img src="LOGO.jpeg" alt=""></a></li>
+                        <li><a href="Menu.html">Menu</a></li>
+                        <li><a href="Reservas.html">Reservas</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <br>
+            
+            <div class="header-content container">
+                <div class="header-img">
+                    <img src="" alt="">
+                </div>
+                <div class="header-txt">
+                    <h1>Doña Hilda</h1>
+                    <p>Doña Hilda Tapas & Grill es un encantador restaurante culinario que abrió sus puertas el 26 de septiembre de 2021 con el objetivo de honrar las tradiciones gastronómicas de la República Dominicana, y los sabores españoles, creando un espacio donde la comunidad pueda disfrutar de sabores auténticos y únicos. Inspirado en el legado culinario de la señora Hilda, madre de la dueña del restaurante. Doña Hilda Tapas and Grill fue creado con una visión audaz y pasión por la cocina según las tradiciones familiares.</p>
+                </div>
+               
+            </div>
+        </header>
+
+        <section class="popular">
+
+            <div class="popular-content container">
+                <h2>Un espacio para compratir  y pasar lindos momentos</h2>
+
+                <p>Doña Hilda Tapas and Grill, cuenta con una terraza al aire libre y también con un espacio más privado con un área cerrada con aire acondicionado brinda comodidad sin sacrificar la atmósfera con luces tenues y la decoración cuidadosamente elegida que crean un ambiente elegante y acogedor, este lugar es perfecto para cenas más formales, reuniones especiales, también tiene la opción de bufé y la opción a la carta inspiradas en la cocina dominicana y sabores españoles. también cuenta con el mini bar ofrece una selección de cócteles y bebidas para acompañar la experiencia. !</p>
+                <div class="popular-pl">
+                    <div class="popular-1">
+                        <img src="Doña Hilda/Local/Airelibre.jpeg" alt="">
+                        <h3>Al aire libre</h3>
+                        <p>
+                            ipsum, dolor sit amet consectetur ad
+                        </p>
+                    </div>
+
+                    <div class="popular-1">
+                        <img src="Doña Hilda/Local/Privado.jpeg" alt="">
+                        <h3>Un espacio mas privado</h3>
+                        <p>
+                            ipsum, dolor sit amet consectetur adi
+                        </p>
+                    </div>
+
+                    <div class="popular-1">
+                        <img src="Doña Hilda/Local/HildaExpress.jpeg" alt="">
+                        <h3>Hilda Express </h3>
+                        <p>
+                            ipsum, dolor sit amet consectetur ad
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="about">
+
+            
+
+            <div class="about-content container">
+                <div class="about-img">
+                    <img src="Doña Hilda/Local/Nosotros.jpeg" alt="">
+    
+                </div>
+    
+                <div class="about-txt">
+                    <h2>Nosotros</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam et exercitationem soluta ab neque omnis rerum temporibus nobis, quaerat cum tenetur, quae aliquid velit laudantium labore quisquam, consequuntur molestias excepturi?</p>
+                    <a href="#" class="btn-1">Informacion</a>
+    
+                </div>
+            </div>
+            
+            
+        </section>
+
+        <section class="number">
+
+            <div class="number-container container">
+                <h2>Menu</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maiores adipisci ab minima eius obcaecati totam cum quasi sint animi numquam iste recusandae eos odit molestiae eaque cupiditate aliquid fugit.</p>
+
+                <div class="number-box">
+                    <div class="number-circle">
+                    
+                        <a href="Menu.html"> A la carta</a>
+                    </div>
+
+                    <div class="number-circle">
+                        <a href="Menu.html">Bebidas</a>
+                      
+                    </div>
+
+                    <div class="number-circle">
+                        <a href="Menu.html">Buffet</a>
+                       
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="contact">
+
+            <div class="contact-content container">
+                <h2>Contacto</h2>
+                <p>Lorem ip.</p>
+
+                <form>
+                    <input type="text" placeholder="Escriba su correo aqui">
+                    <input type="submit" value="Enviar" class="btn-2">
+                </form>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="footer-content container">
+                <div class="link">
+                    <h3>Redes sociales</h3>
+                    <ul>
+                        <li><a href="https://instagram.com/donahildabani?igshid=MmU2YjMzNjRlOQ==">Instagram</a></li>
+                        <li><a href="https://api.whatsapp.com/message/XV75XSG4HTO2J1?autoload=1&app_absent=0
+                            ">WhatsApp</a></li>
+                        <li><a href="https://www.facebook.com/DonaHildaBani?mibextid=ZbWKwL">Facebook</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+</body>
+</html>
