a43626416d1eb791e2f7244fd89cc27cec183d0f
Nahomi Núñez <sotonahomi@gmail.com>
2023-09-19 21:03:14 -0400

    Vista de "Nosotrps"

---
+++ Vistas/Nosotros.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous" ></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="icon" href="img/odontopolis.png">
+        <link rel="stylesheet" type="text/css" href="./Css/style.css">
+
+        <title> Odontopolis RD </title>
+
+    </head>   
+
+    <body>
+
+        <article style="padding-top: 100px" id ="Inicio"> 
+
+            <div class="container-lg my-15">
+
+                <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+                    <!-- Carousel indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+                    </ol>
+        
+                    <!-- Wrapper for carousel items -->
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="img/ejemplo.jpg" class="d-block w-100" alt="Slide 1">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some demonstrative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img src="img/ejemplo.jpg" class="d-block w-100" alt="Slide 2">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some demonstrative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+
+                        <div class="carousel-item">
+                            <img src="img/ejemplo.jpg" class="d-block w-100" alt="Slide 3">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some demonstrative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Carousel controls -->
+                    <a class="carousel-control-prev" href="#myCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#myCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+
+                </div>
+            
+            </div>
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <div class="card text-center ">
+                            <div class="card-body shadow">
+                                <p class="lead fw-normal mb-0 me-3 text-center">
+                                    Somos una clínica odontológica comprometida con el bienestar y la satisfacción de nuestros pacientes
+                                </p>
+                                <p class="lead fw-normal mb-0 me-3 text-center">
+                                    Visitanos en:
+                                </p>
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-md-6">
+                                            <p class="card-texttext-center"> <b> Odontópolis RD (Gazcue) </b></p>
+                                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15137.525808602295!2d-69.9102256!3d18.466373!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf8987bfdb7eaf%3A0x8306f0d2c9928bb!2sOdont%C3%B3polis%20RD%20(Gazcue)!5e0!3m2!1ses!2sdo!4v1686253846668!5m2!1ses!2sdo" 
+                                            width="500" height="300" style="border:0;" allowfullscreen="" 
+                                            loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                            </iframe>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="card-text text-center"> <b> Odontópolis RD (Bani) </b></p>
+                                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7577.012582364594!2d-70.34201381938549!3d18.27844067560724!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ea54da70f8e3329%3A0x29b96f5d5b3139ed!2sOdont%C3%B3polis%20RD%20(Bani)!5e0!3m2!1ses!2sdo!4v1686253679875!5m2!1ses!2sdo" 
+                                            width="500" height="300" style="border:0;" allowfullscreen="" 
+                                            loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                            </iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>     
+      
+        </article>
+    </body>
+</html>
