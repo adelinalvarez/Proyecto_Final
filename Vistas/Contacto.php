7974b5e3cf23e44cec3832195dcc50027b58ab63
Nahomi Núñez <sotonahomi@gmail.com>
2023-09-23 23:45:54 -0400

    Vista de contacto

---
+++ Vistas/Contacto.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Contacto</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="../assets/css/style.css" rel="stylesheet">
+        <link rel="icon" href="../Imagenes/Logo.png">
+
+    </head>
+
+    <body>
+        <br>
+
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top " style="background-color: #FFEC6F">
+            <div class="container d-flex align-items-center">
+
+            <h1 class="logo me-auto"><a href="index.html"> <img src="../Imagenes/Logo.png"></a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                <li><a class="nav-link scrollto" href="#hero">Inicio</a></li>
+                <li><a class="nav-link scrollto" href="Nosotros.php">Nosotros</a></li>
+                <li><a class="nav-link scrollto" href="#about">Menu</a></li>
+                <li><a class="nav-link scrollto" href="#services">Reserva</a></li>
+                <li><a class="nav-link   scrollto" href="#portfolio">Contacto</a></li>
+                <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+            </div>
+        </header><!-- End Header -->
+
+        <main id="main">
+
+            <!-- ======= Contact Section ======= -->
+            <section id="contact" class="contact">
+                <div class="container" data-aos="fade-up">
+
+
+                    <div class="row">
+
+                    <div class="col-lg-5 d-flex align-items-stretch">
+                        <div class="info">
+                            <div class="address">
+                                <i class="bi bi-geo-alt"></i>
+                                <h4>Ubicación:</h4>
+                                <p>16 De Agosto Esq, Baní 94000</p>
+                            </div>
+
+                            <div class="email">
+                                <i class="bi bi-envelope"></i>
+                                <h4>Correo:</h4>
+                                <p>info@example.com</p>
+                            </div>
+
+                            <div class="Telefono">
+                                <i class="bi bi-phone"></i>
+                                <h4>Telefono:</h4>
+                                <p>+1 809-522-4151</p>
+                            </div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15154.1395834592!2d-70.3335576!3d18.2771309!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ea54f0ac5c34d01%3A0x6f86dcf823593705!2sDo%C3%B1a%20Hilda%20Tapas%20%26%20Grill!5e0!3m2!1ses!2sdo!4v1695340721703!5m2!1ses!2sdo" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                            <label for="name">Nombre</label>
+                            <input type="text" name="name" class="form-control" id="name" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                            <label for="name">Correo</label>
+                            <input type="email" class="form-control" name="email" id="email" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Asunto</label>
+                            <input type="text" class="form-control" name="subject" id="subject" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Mensaje</label>
+                            <textarea class="form-control" name="message" rows="10" required></textarea>
+                        </div>
+                        <div class="my-3">
+                            <div class="loading">Cargando</div>
+                            <div class="error-message"></div>
+                            <div class="sent-message">Enviado correctamente</div>
+                        </div>
+                        <div class="text-center" ><button type="submit" style="background-color: #FFEC6F; color: black">Enviar</button></div>
+                        </form>
+                    </div>
+
+                    </div>
+
+                </div>
+            </section><!-- End Contact Section -->
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-6 col-md-6 footer-contact">
+                        <h3>Doña Hilda Tapas and Grill</h3>
+                        <p>
+                        A108 Adam Street <br>
+                        New York, NY 535022<br>
+                        United States <br><br>
+                        <strong>Phone:</strong> +1 5589 55488 55<br>
+                        <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+                                
+                    <div class="col-lg-6 col-md-6 footer-links">
+                        <h4>Our Social Networks</h4>
+                        <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+                        <div class="social-links mt-3">
+                        <a href="#" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </main><!-- End #main -->
+
+        <!-- ======= Footer ======= -->
+        <footer id="footer">
+
+            <div class="container footer-bottom clearfix">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+                </div>
+            </div>
+        </footer><!-- End Footer -->
+
+        <!-- Vendor JS Files -->
+        <script src="../assets/vendor/aos/aos.js"></script>
+        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+        <script src="../assets/vendor/php-email-form/validate.js"></script>
+        <script src="../assets/js/main.js"></script>
+
+        
+    </body>
+</html>
--- Vistas/Nosotros.php
+++ Vistas/Nosotros.php
@@ -3,61 +3,56 @@
     
     <head>
 
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous" ></script>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="icon" href="img/odontopolis.png">
-        <link rel="stylesheet" type="text/css" href="./Css/style.css">
-        <link rel="icon" href="../Imagenes/Logo.png">
-
         <title>Nosotros</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="../assets/css/style.css" rel="stylesheet">
+        <link rel="icon" href="../Imagenes/Logo.png">
 
     </head>   
 
-    
+    <body>
 
-    <header style="position: fixed; width: 100%; z-index: 999">
-            <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <!-- Container wrapper -->
-                <div class="container" >
-                    <!-- Navbar brand -->
-                    <a class="navbar-brand me-2" href="../index.php">
-                        <img src="../Imagenes/Logo.png" height="60" width ="60" style="margin-top: -1px;" />
-                    </a>
-                    <!-- Toggle button -->
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarButtonsExample" aria-controls="navbarButtonsExample" aria-expanded="false" aria-label="Toggle navigation" >
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <!-- Collapsible wrapper -->
-                    <div class="collapse navbar-collapse" id="navbarButtonsExample">
-                        <!-- Left links -->
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 350px;">
-                            <li class="nav-item">
-                                <a class="nav-link" href="../index.php"> Inicio </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Menu.php"> Menu </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Reservas.php"> Reserva </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="Contacto.php"> Contacto </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- Collapsible wrapper -->
-                </div>
-                <!-- Container wrapper -->
-            </nav>
-        </header>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top " style="background-color: #FFEC6F">
+            <div class="container d-flex align-items-center">
+
+            <h1 class="logo me-auto"><a href="index.html"> <img src="../Imagenes/Logo.png"></a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                <li><a class="nav-link scrollto" href="#hero">Inicio</a></li>
+                <li><a class="nav-link scrollto" href="Nosotros.php">Nosotros</a></li>
+                <li><a class="nav-link scrollto" href="#about">Menu</a></li>
+                <li><a class="nav-link scrollto" href="#services">Reserva</a></li>
+                <li><a class="nav-link   scrollto" href="#portfolio">Contacto</a></li>
+                <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+            </div>
+        </header><!-- End Header -->
 
         <article style="padding-top:  90px"> 
 
@@ -221,72 +216,54 @@
                 </div>
             </div>     
             <br>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12">
-                        <div class="card text-center ">
-                            <div class="card-body shadow" style="background-color:#FFEC6F">
-                                <div class="container">
-                                    <div class="row ">
-                                        <p> 
-                                            <strong> 
-                                                Visitanos en:
-                                            </strong>
-                                        </p>
-                                        <div class="col-md-6 mapa-responsive">
-                                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15154.1395834592!2d-70.3335576!3d18.2771309!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ea54f0ac5c34d01%3A0x6f86dcf823593705!2sDo%C3%B1a%20Hilda%20Tapas%20%26%20Grill!5e0!3m2!1ses!2sdo!4v1695340721703!5m2!1ses!2sdo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>     
-            <br>
         </article>
 
-        <!-- Footer -->
-        <footer class="text-center text-white" style="background-color:#FFFFFF">
-            <!-- Grid container -->
-            <div class="container p-4">
+        <div class="container">
+                <div class="row">
 
-                <!-- Section: Text -->
-                <section class="mb-4">
-                    <p>
-                        Eslogan
-                    </p>
-                    </section>
-                    <!-- Section: Text -->
-                <!-- Section: Social media -->
-                <section class="mb-4">
+                    <div class="col-lg-6 col-md-6 footer-contact">
+                        <h3>Doña Hilda Tapas and Grill</h3>
+                        <p>
+                        A108 Adam Street <br>
+                        New York, NY 535022<br>
+                        United States <br><br>
+                        <strong>Phone:</strong> +1 5589 55488 55<br>
+                        <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+                                
+                    <div class="col-lg-6 col-md-6 footer-links">
+                        <h4>Our Social Networks</h4>
+                        <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+                        <div class="social-links mt-3">
+                        <a href="#" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        </div>
+                    </div>
 
-                <!-- Facebook -->
-                <a href="#!" role="button"><i class="fa fa-facebook-square" style="font-size:36px; background-color: #FFFFFF;"></i
-                ></a>
-                
-                 <!-- WhatsApp -->
-                <a href="#!" role="button"><i class="fa fa-whatsapp" style="font-size:36px; background-color: #FFFFFF;"></i ></a>
-                
-                <!-- Instagram -->
-                <a href="#!" role="button"><i class="fa fa-instagram" style="font-size:36px; background-color: #FFFFFF;"></i
-                ></a>
-                
-               
-                </section>
-                <!-- Section: Social media -->
-
-
+                </div>
             </div>
-            <!-- Grid container -->
 
-            <!-- Copyright -->
-            <div class="text-center p-3" style="background-color: #D5D5D5;">
-                 
-               <P style= "text-color:black">© 2023 Copyright: Doña Hilda  </P>
+        <!-- ======= Footer ======= -->
+        <footer id="footer">
+
+            <div class="container footer-bottom clearfix">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+                </div>
             </div>
-            <!-- Copyright -->
-            </footer>
+        </footer><!-- End Footer -->
             <!-- Footer -->
+
+            <!-- Vendor JS Files -->
+        <script src="../assets/vendor/aos/aos.js"></script>
+        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+        <script src="../assets/vendor/php-email-form/validate.js"></script>
+        <script src="../assets/js/main.js"></script>
     </body>
 </html>
