8fb9b3816980ca612f36ed2c0eeb8493e3018287
adelinalvarez <144082099+adelinalvarez@users.noreply.github.com>
2023-09-25 22:14:56 -0400

    vistas

--- Vistas/Nosotros.php
+++ Vistas/Nosotros.php
@@ -16,6 +16,11 @@
         <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
         <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
         <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous" ></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Template Main CSS File -->
         <link href="../assets/css/style.css" rel="stylesheet">
         <link rel="icon" href="../Imagenes/Logo.png">
@@ -45,7 +50,9 @@
         <!-- End Header -->
 
         <article style="padding-top:  90px"> 
-
+                    <div class="section-title">
+                      <h2>Nosotros</h2>
+                    </div>
             <div class="container-lg my-15">
 
                 <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
