b13bd2f1ad9ce043dccb39716bb2656514f977b5
adelinalvarez <144082099+adelinalvarez@users.noreply.github.com>
2023-09-28 22:02:52 -0400

    Estilo

--- Index.php
+++ Index.php
@@ -41,25 +41,55 @@
             </div>
         </header>
         <!-- End Header -->
-  <!--Video Section-->
-
-    <section class="content-section video-section fables-after-overlay">
-        <div class="pattern-overlay">
-            <video width="100%" controls autoplay>
-                <source src="../Video_inicio.mp4">
-                Tu navegador no soporta la reproducción de video.
-            </video>
-            <div class="container position-relative z-index">
-                <div class="fables-video-caption text-center wow zoomIn" data-wow-delay="1s" data-wow-duration="2s">
-                    <h1 class="white-color font-weight-bold mt-6">Somos una agencia de productos digitales de servicio completo</h1>  
-                    <p class="fables-fifth-text-color mt-3 mb-4 font-18">
-                        Somos una agencia de productos digitales de servicio completo
-                    </p>
-                </div>
+        <article style="padding-top:  90px"> 
+            <div class="section-title">
+                <h2>Nosotros</h2>
             </div>
-        </div>
-    </section>
-<!--Video Section Ends Here--> 
+            <div class="container-lg my-15">
+                <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+                    <!-- Carousel indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+                        <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+                    </ol>
+                    <!-- Wrapper for carousel items -->
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="Imagenes/Local/Airelibre.jpeg" class="d-block w-100" alt="Slide 1">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some demonstrative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Imagenes/Local/Airelibre.jpeg" class="d-block w-100" alt="Slide 2">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some demonstrative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Imagenes/Local/Airelibre.jpeg" class="d-block w-100" alt="Slide 3">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some demonstrative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Carousel controls -->
+                    <a class="carousel-control-prev" href="#myCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#myCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
+                </div>
+                <br>
+            </div>
+                 
+        </article>
         <section class="popular">
 
             <div class="popular-content container">
